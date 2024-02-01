321a5724a2596aed0dc6ab2bdfc21c0fb35179b3
Svetlana Zhuravleva <karpenter.xo@gmail.com>
2024-02-01 11:26:45 +0300

    lessons

---
+++ Frontend/lesson(30.01 JS)/script.js
@@ -0,0 +1,77 @@
+// let username = 'Svetlana';
+
+// let num = 10;
+// console.log(num);
+
+// num = 50;
+// console.log(num);
+
+// const username = prompt('Kaka zovut');
+// console.log(username);
+
+// const r = 50;
+// const g = 70;
+// const b = 80;
+
+// console.log(`rgb(${r}, ${g}, ${b})`);
+
+// const name = prompt('Как вас зовут?');
+
+// alert(`Пользователь: ${name}`);
+
+// const yearBirth = prompt('В каком году вы роделись?');
+// const years = 2023 - yearBirth;
+// alert(`Вам ${years} лет`);
+
+// const number = prompt('Введите число');
+// console.log(number ** 2);
+
+// const degree = 10;
+
+// if (degree > 15 && degree < 30) {
+// 	console.log('надень футболку');
+// } else if (degree === 15) {
+// 	console.log('сам выбери');
+// } else if (degree > 30) {
+// 	console.log('останься дома');
+// } else {
+// 	console.log('надень куртку');
+// }
+
+// const number = +prompt('Введите число');
+
+// if (number > 100) {
+// 	console.log('Больше');
+// } else if (number === 100) {
+// 	console.log('Равно');
+// } else {
+// 	console.log('Меньше');
+// }
+
+// const sum = 10;
+
+// if (sum > 1500) {
+// 	console.log('Ваша скидка 15%');
+// } else if (sum > 1000) {
+// 	console.log('Ваша скидка 10%');
+// } else {
+// 	console.log('Нет скидки');
+// }
+
+// const number = 5;
+
+// if (number % 2) {
+// 	console.log('Нечетное');
+// } else {
+// 	console.log('Четное');
+// }
+
+const number = 2;
+
+if (number > 0) {
+	console.log('Положительно');
+} else if (number < 0) {
+	console.log('Отрицательно');
+} else {
+	console.log('Ноль');
+}
